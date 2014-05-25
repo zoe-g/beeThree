9cd340db5394b88021b9e7e007867b0781234eba
Zoe <zoe.goldfarb@gmail.com>
2014-05-25 13:58:16 -0700

    set up controller and formatted html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
 	
 	def show
-		#@user_transactions = @current_user.users_products.users_products
-		
+		@sell_transactions = UsersProducts.where(user_id: params[:id]).where(role_id: 1)
+		# @buyers = @sell_transactions.products
+
+		@buy_transactions = UsersProducts.where(user_id: params[:id]).where(role_id: 2)
+
 	end
 
 	def create
