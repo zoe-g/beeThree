1823375e23aa1b7122a9e3dcbfb138025182b6b8
timsanty <timsanty@me.com>
2014-05-25 13:28:22 -0700

    s3 implementation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,34 @@
 class ProductsController < ApplicationController
+	# This method associates the attribute ":avatar" with a file attachment
+		  has_attached_file :avatar, styles: {
+		    thumb: '100x100>',
+		    square: '200x200#',
+		    medium: '300x300>'
+		  }
 
+	# Validate the attached image is image/jpg, image/png, etc
+  		validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+	
 	def index
     @products = Product.all
 	end
 
 	def new
-		@products = Products.all
-    # @product = Product.new
 	end
 	
-  def create
-    @product = Product.create(product_params)
+	def create
+	    @product = Product.new(product_params)
+
+	    if @product.save
+	      redirect_to @product, notice: 'Item was successfully created.' }
+	     else
+	       render action: 'new'
+	    end
   end
 
   private
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :image_url)
-  end
+	  def product_params
+	    params.require(:product).permit(:avatar, :name, :description, :price)
+	  end
 end
+
