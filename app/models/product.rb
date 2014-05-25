551ef8e93d67a97d6198bd89d4b71110650fb82f
daveyk535 <dave@phase2minds.com>
2014-05-25 13:36:29 -0700

    created basic tests for the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ActiveRecord::Base
   belongs_to :status
 
-  validates :name, presence: true, length: {minimum: 2, maximum: 20}
-  # validates :description, presence: true length: {minimum: 2, maximum: 200}
-  # validates :price, presence: true
-  # validates :url, presence: true
+  validates :name, presence: true, length: {minimum: 2, maximum: 50}
+  validates :description, presence: true, length: {minimum: 2, maximum: 200}
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 3000 }
 
 end
+
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,15 +1,70 @@
 require 'spec_helper'
 
-describe Product do
-  
-  it 'should create a new instance of Product if given valid attributes' do
-      Product = Product.create(name: "sofa", description: "used couch, some stains", price: 50, image_url: "http://i.imgur.com/A6nmz.jpg")
-      Product.should be_valid
+describe 'product' do
+
+  before(:each) do      
+    @product = Product.create(name: "sofa", description: "used couch, some stains", price: 50, image_url: "http://i.imgur.com/A6nmz.jpg")
   end
 
-  it 'should have a name' do
-    Product = Product.create(description: "used couch, some stains", price: 50, image_url: "http://i.imgur.com/A6nmz.jpg")
-      Product.should_not be_valid
+  it 'should create a new instance of Product if given valid attributes' do
+    @product.should be_valid
+  end
+
+  describe 'name' do
+
+    it 'should have a name' do
+      @product.name = nil
+      @product.should_not be_valid
+    end
+
+    it 'should be at least two characters' do
+      product = Product.create(name: "a")
+      product.should_not be_valid
+    end
+
+  end
+
+  describe 'description' do
+
+    it 'should have a description' do
+      @product.description = nil
+      @product.should_not be_valid
+    end
+
+    it 'should be at least two characters' do
+      @product.description = "a"
+      @product.should_not be_valid
+    end
+
+  end
+
+  describe 'price' do
+
+    it 'should have a price' do
+      @product.price = nil
+      @product.should_not be_valid
+    end
+
+    it 'should be an integer' do
+      @product.price = 50.8
+      @product.should_not be_valid
+    end
+
+    # it 'should be an integer' do
+    #   @product.price = '50'
+    #   @product.should_not be_valid
+    # end
+
+    it 'should be greater than zero' do
+      @product.price = 0
+      @product.should_not be_valid
+    end
+
+    it 'should be less than three thousand' do
+      @product.price = 3001
+      @product.should_not be_valid
+    end
+
   end
 
 end
