359f4278eccf23cba15d36451c4e0d037b98fd50
courtney mcilhenny <github@courtneymcilhenny>
2014-05-26 15:01:15 -0700

    adding field to grab user's email from facebook login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :facebook_id)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.facebook_id = auth.uid
+      user.email = auth.info.email
       user.full_name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
