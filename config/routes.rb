5f92d5b6c70256e767d391c0a6fd925932a07a3d
courtney mcilhenny <github@courtneymcilhenny>
2014-05-26 12:31:26 -0700

    updated routes for facebook login. Added a sessions controller to handle logging in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+
+  #setting up routes for facebook login based on RailsCast:
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'  
+
+
   resources :products
   resources :users, only: [:show, :create, :update]
   
