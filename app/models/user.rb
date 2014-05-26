27e51cbdd58fa69d7c71d6ff52c861911243f126
courtney mcilhenny <github@courtneymcilhenny>
2014-05-26 13:52:17 -0700

    updating user model for facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
 	has_many :users_products
 	has_many :products, through: :users_products
 
+  #adding omniauth code for user sign-in from RailsCast
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.facebook_id = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
