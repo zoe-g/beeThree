3a5c842ab3b1f270133e588ee3225965529d2aa8
courtney mcilhenny <github@courtneymcilhenny>
2014-05-26 16:05:42 -0700

    updating user model to grab location and image info from facebook once user signs in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.facebook_id = auth.uid
       user.email = auth.info.email
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.location = auth.info.location
+      user.image = auth.info.image
       user.full_name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
@@ -16,3 +20,4 @@ class User < ActiveRecord::Base
     end
   end
 end
+
