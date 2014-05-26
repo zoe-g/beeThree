7ecad57a29564026f64de629e9c3d8568ec1ab3d
courtney mcilhenny <github@courtneymcilhenny>
2014-05-26 14:08:41 -0700

    fixing column references

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
 
   #adding omniauth code for user sign-in from RailsCast
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+    where(auth.slice(:provider, :facebook_id)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.facebook_id = auth.uid
-      user.name = auth.info.name
+      user.full_name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
