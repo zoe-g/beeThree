bf5dbe945c43c742b28b937ddaed51388571d4ac
courtney mcilhenny <github@courtneymcilhenny>
2014-05-26 14:09:12 -0700

    updating callback to be a 'get'.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   #setting up routes for facebook login based on RailsCast:
-  match 'auth/:provider/callback', to: 'sessions#create', via: 'post'
+  match 'auth/:provider/callback', to: 'sessions#create', via: 'get'
   match 'auth/failure', to: redirect('/'), via: 'get'
   match 'signout', to: 'sessions#destroy', as: 'signout', via: 'delete'
 
