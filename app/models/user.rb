12cad3946a9b5a6b88a920c17a92fab00c2fe0cb
courtney mcilhenny <github@courtneymcilhenny>
2014-05-27 10:57:11 -0700

    updating user model to include uid to accurately pull facebook data.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,24 @@ class User < ActiveRecord::Base
 
   #adding omniauth code for user sign-in from RailsCast
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :facebook_id)).first_or_initialize.tap do |user|
+puts auth
+
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
-      user.facebook_id = auth.uid
+      user.uid = auth.uid
+      
+
+
+
       user.email = auth.info.email
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.location = auth.info.location
       user.image = auth.info.image
       user.full_name = auth.info.name
+
+
+      
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
