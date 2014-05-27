61bb54b5e42cc33f8f0cc353ecf3593ecd3cbbc3
courtney mcilhenny <github@courtneymcilhenny>
2014-05-27 11:27:20 -0700

    updating schema for users to include uid.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526225136) do
+ActiveRecord::Schema.define(version: 20140527181829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20140526225136) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "provider"
-    t.string   "facebook_id"
+    t.string   "uid"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
     t.string   "full_name"
