a4804c8a0c88da18e71b7ec5963f4b1c286ee8b7
courtney mcilhenny <github@courtneymcilhenny>
2014-05-27 11:30:03 -0700

    fixing spacing in user model to look nice

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,12 @@ puts auth
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      
-
-
-
       user.email = auth.info.email
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.location = auth.info.location
       user.image = auth.info.image
       user.full_name = auth.info.name
-
-
-      
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
