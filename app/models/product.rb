45466042636843857f490a202d141eb25a333cf6
timsanty <timsanty@me.com>
2014-05-28 15:10:26 -0700

    fixed typo with paperclip user avatar, changed to product avatar

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,12 @@
 class Product < ActiveRecord::Base
 
-  has_many :users, through: :users_products
-  belongs_to :status
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
+  has_many :users, through: :users_products
+  belongs_to :status
+
 
   validates :name, presence: true, length: {minimum: 2, maximum: 50}
   validates :description, presence: true, length: {minimum: 2, maximum: 200}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140528185346) do
+ActiveRecord::Schema.define(version: 20140528215829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 20140528185346) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "products", ["product_status_id"], name: "index_products_on_product_status_id", using: :btree
@@ -60,6 +63,10 @@ ActiveRecord::Schema.define(version: 20140528185346) do
     t.string   "full_name"
     t.string   "location"
     t.string   "image"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users_products", force: true do |t|
