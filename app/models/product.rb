1c6f7075e28f3f14d0bc0cecf843e4c33917087f
courtney mcilhenny <github@courtneymcilhenny>
2014-05-28 12:57:11 -0700

    fixing merge errors

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
   belongs_to :status
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   validates :name, presence: true, length: {minimum: 2, maximum: 50}
   validates :description, presence: true, length: {minimum: 2, maximum: 200}
