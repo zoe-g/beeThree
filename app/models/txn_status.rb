fc15e621b8085109fc07cb02de3e7f2e867bcd40
Zoe <zoe.goldfarb@gmail.com>
2014-05-27 19:16:09 -0700

    reversing some model methods we don't need

--- app/models/txn_status.rb
+++ app/models/txn_status.rb
@@ -1,19 +1,11 @@
 class TxnStatus < ActiveRecord::Base
 
-	def open
-		self.find_by(name: 'Listed').id
-	end
-
 	def open?
 		if self.name == 'Listed'
 			return true
 		else
 			return false
 		end
-	end
-
-	def pending
-		TxnStatus.find_by(name: 'Sale Pending').id
 	end
 
 	def pending?
@@ -24,10 +16,6 @@ class TxnStatus < ActiveRecord::Base
 		end
 	end
 
-	def cancelled
-		TxnStatus.find_by(name: 'Cancelled').id
-	end
-
 	def cancelled?
 		if self.name == 'Sale Cancelled'
 			return true
@@ -36,20 +24,12 @@ class TxnStatus < ActiveRecord::Base
 		end
 	end
 
-	def payment
-		TxnStatus.find_by(name: 'Payment Pending').id
-	end
-
 	def payment?
 		if self.name == 'Payment Pending'
 			return true
 		else
 			return false
 		end
-	end
-
-	def complete
-		TxnStatus.find_by(name: 'Sale Complete').id
 	end
 
 	def complete?
